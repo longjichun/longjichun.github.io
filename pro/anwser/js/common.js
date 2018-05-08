c7edfe1fcf07a2d166f312fd232ffb06cf80dded
龙及纯 <longjichun@longjichundeMac-mini.local>
2018-05-08 14:55:06 +0800

    anwser

--- pro/anwser/js/common.js
+++ pro/anwser/js/common.js
@@ -35,7 +35,7 @@ if (appversion && appversion != "null") { //客户端内判断依据
                 if ((typeof jsBridge) != "undefined") { //客户端内不处理
                     isApp = true;
                 } else { //客户端外强制跳转活动分享页
-                    window.location.href = "share.html";
+                    // window.location.href = "share.html";
                 }
             } else { //iOS不需要特殊判断
                 isApp = true;
@@ -48,7 +48,7 @@ if (appversion && appversion != "null") { //客户端内判断依据
     var longurl = window.location.href;
     if (longurl.indexOf("share.html") > -1) { //三级主界面不跳转
     } else {
-        window.location.href = "share.html";
+        //window.location.href = "share.html";
     }
 }
 
@@ -151,52 +151,52 @@ $.fn.visibleHeight = function() {
 
 
 
-(function(designWidth, maxWidth) {     //PX rem转换
-    var doc = document,
-        win = window,
-        docEl = doc.documentElement,
-        remStyle = document.createElement("style"),
-        tid;
+// (function(designWidth, maxWidth) {     //PX rem转换
+//     var doc = document,
+//         win = window,
+//         docEl = doc.documentElement,
+//         remStyle = document.createElement("style"),
+//         tid;
 
-    function refreshRem() {
-        var width = docEl.getBoundingClientRect().width;
-        maxWidth = maxWidth || 540;
-        width>maxWidth && (width=maxWidth);
-        var rem = width * 100 / designWidth;
-        remStyle.innerHTML = 'html{font-size:' + rem + 'px;}';
-    }
+//     function refreshRem() {
+//         var width = docEl.getBoundingClientRect().width;
+//         maxWidth = maxWidth || 540;
+//         width>maxWidth && (width=maxWidth);
+//         var rem = width * 100 / designWidth;
+//         remStyle.innerHTML = 'html{font-size:' + rem + 'px;}';
+//     }
 
-    if (docEl.firstElementChild) {
-        docEl.firstElementChild.appendChild(remStyle);
-    } else {
-        var wrap = doc.createElement("div");
-        wrap.appendChild(remStyle);
-        doc.write(wrap.innerHTML);
-        wrap = null;
-    }
-    //要等 wiewport 设置好后才能执行 refreshRem，不然 refreshRem 会执行2次；
-    refreshRem();
+//     if (docEl.firstElementChild) {
+//         docEl.firstElementChild.appendChild(remStyle);
+//     } else {
+//         var wrap = doc.createElement("div");
+//         wrap.appendChild(remStyle);
+//         doc.write(wrap.innerHTML);
+//         wrap = null;
+//     }
+//     //要等 wiewport 设置好后才能执行 refreshRem，不然 refreshRem 会执行2次；
+//     refreshRem();
 
-    win.addEventListener("resize", function() {
-        clearTimeout(tid); //防止执行两次
-        tid = setTimeout(refreshRem, 300);
-    }, false);
+//     win.addEventListener("resize", function() {
+//         clearTimeout(tid); //防止执行两次
+//         tid = setTimeout(refreshRem, 300);
+//     }, false);
 
-    win.addEventListener("pageshow", function(e) {
-        if (e.persisted) { // 浏览器后退的时候重新计算
-            clearTimeout(tid);
-            tid = setTimeout(refreshRem, 300);
-        }
-    }, false);
+//     win.addEventListener("pageshow", function(e) {
+//         if (e.persisted) { // 浏览器后退的时候重新计算
+//             clearTimeout(tid);
+//             tid = setTimeout(refreshRem, 300);
+//         }
+//     }, false);
 
-    if (doc.readyState === "complete") {
-        doc.body.style.fontSize = "16px";
-    } else {
-        doc.addEventListener("DOMContentLoaded", function(e) {
-            doc.body.style.fontSize = "16px";
-        }, false);
-    }
-})(750, 750);
+//     if (doc.readyState === "complete") {
+//         doc.body.style.fontSize = "16px";
+//     } else {
+//         doc.addEventListener("DOMContentLoaded", function(e) {
+//             doc.body.style.fontSize = "16px";
+//         }, false);
+//     }
+// })(750, 750);
 
 /* 倒计时计算函数 */
 function getTime(startTime) {
